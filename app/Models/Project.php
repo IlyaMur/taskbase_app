795f15ef5fbcba9935fa288f7835288a20049feb
Ilya Mur <ilyamur@hotmail.com>
2022-02-28 16:01:39 +0300

    add activity && record by observer

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
     use HasFactory;
+
+    public function activity()
+    {
+        return $this->hasMany(Activity::class);
+    }
 
     protected $guarded = [];
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+
 Route::group(['middleware' => 'auth'], function () {
     Route::controller('ProjectController')->group(function () {
         Route::get('/', 'index');
